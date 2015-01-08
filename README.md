bc8d9c0f871c1db6a2e1c994afc943806919f981
Mitul Golakiya <me@mitul.me>
2015-01-08 19:20:29 +0530

    Cleanup

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Meteor Chat App
 Test Meteor Chat App with material UI which supports basic Chatting with user online/offline status
+
+# Dependencies
+App uses following packages
+
+* iron:router
+*  accounts-password
+* momentjs:moment
+* joshowens:accounts-entry
+* fezvrasta:bootstrap-material-design
+* mizzao:user-status
--- libs/router.js
+++ libs/router.js
@@ -2,21 +2,6 @@
  * Created by Mitul on 30/12/14.
  */
 
-//Router.route('/sign-in', function () {
-//    this.render('login');
-//});
-//
-//Router.route('/sign-out', function () {
-//    Meteor.logout(function () {
-//        Router.go('/');
-//    });
-//
-//});
-//
-//Router.route('/sign-up', function () {
-//    this.render('singUp');
-//});
-
 Router.route("/", function () {
     this.redirect('chat');
 });
@@ -27,37 +12,6 @@ Router.route("home", function () {
     else
         this.redirect('sign-in');
 });
-//
-//Router.route("/login", function () {
-//    if (Meteor.user()) {
-//        this.redirect('/chat');
-//        this.render("chat");
-//    }
-//    else
-//        this.render('login');
-//});
-//
-//Router.route("/signUp", function () {
-//    if (Meteor.user()) {
-//        this.redirect('/chat');
-//        this.render("chat");
-//    }
-//    else
-//        this.render('signUp');
-//});
-//
-//Router.onBeforeAction(function () {
-//    AccountsEntry.signInRequired(this);
-//}, {except: ['sign-in', 'sign-up']});
-
-//Router.route("/home", {
-//    name: 'home',
-//    path: "/home",
-//    template: 'home',
-//    action: function () {
-//        this.render('home');
-//    }
-//});
 
 Router.route("chat", {
     name: 'chat',
@@ -70,9 +24,3 @@ Router.route("chat", {
         this.render('chat');
     }
 });
-
-//Router.route("/logout", function () {
-//    Meteor.logout(function () {
-//        Router.go('/');
-//    });
-//});
